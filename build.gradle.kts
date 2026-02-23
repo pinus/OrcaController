ae97a233973b51084161a685b5b6d463918cf672
pinus <nba01176@nifty.ne.jp>
2026-02-23 11:08:09 +0900

    chore: update Kotlin, Selenium, Logback, and enable Gradle configuration cache
    - Bump Kotlin to 2.3.10 in `.idea/kotlinc.xml`.
    - Upgrade Selenium to 4.41.0 and Logback to 1.5.32 in `build.gradle.kts`.
    - Enable Gradle configuration cache in `gradle.properties`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,9 +80,9 @@ tasks {
 }
 
 dependencies {
-    implementation("org.seleniumhq.selenium:selenium-java:4.40.0")
+    implementation("org.seleniumhq.selenium:selenium-java:4.41.0")
     implementation("io.github.bonigarcia:webdrivermanager:6.3.3")
-    implementation("ch.qos.logback:logback-classic:1.5.28")
+    implementation("ch.qos.logback:logback-classic:1.5.32")
     implementation("commons-io:commons-io:2.21.0")
     implementation("com.formdev:flatlaf:3.7")
 }

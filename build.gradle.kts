6417060337d081896185c5407aa8d62656819d9a
pinus <nba01176@nifty.ne.jp>
2026-08-25 10:46:04 +0900

    chore(build): update dependencies and Gradle wrapper
    - Updated `selenium-java` from version 4.46.0 to 4.47.0.
    - Upgraded `logback-classic` from version 1.6.1 to 1.6.3.
    - Changed the Gradle distribution URL from `gradle-9.6.1-bin.zip` to `gradle-9.7.1-bin.zip`.
    - Adjusted the `conveyor.compatibility-level` from 19 to 22.

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,9 +80,9 @@ tasks {
 }
 
 dependencies {
-    implementation("org.seleniumhq.selenium:selenium-java:4.46.0")
+    implementation("org.seleniumhq.selenium:selenium-java:4.47.0")
     implementation("io.github.bonigarcia:webdrivermanager:6.3.4")
-    implementation("ch.qos.logback:logback-classic:1.6.1")
+    implementation("ch.qos.logback:logback-classic:1.6.3")
     implementation("commons-io:commons-io:2.22.0")
     implementation("com.formdev:flatlaf:3.7.2")
 }
